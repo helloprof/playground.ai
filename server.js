a815a8c28d2465c82e8e1459a77eabc9e872abad
helloprof <suluxan.seneca@gmail.com>
2024-06-03 12:33:13 -0400

    simple route setup for get models and get models by id

--- server.js
+++ server.js
@@ -3,10 +3,37 @@ const express = require("express")
 const app = express()
 const PORT = 8080
 
+const modelService = require("./modules/modelService")
+// modelService.testFx()
+
 app.get("/", (req, res) => {
     res.send("hello world")
 })
 
-app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`)
+app.get("/models", (req, res) => {
+    modelService.getModels().then((models) => {
+        res.json(models)
+    }).catch((err) => {
+        console.log(err)
+    })
+})
+
+app.get("/models/test", (req, res) => {
+    modelService.getModelByID(1).then((modelData) => {
+        res.send(modelData)
+    }).catch((err) => {
+        console.log(err)
+    })
+})
+
+app.get("*", (req, res) => {
+    res.send(404)
+})
+
+modelService.initialize().then(() => {
+    app.listen(PORT, () => {
+        console.log(`listening on port ${PORT}`)
+    })
+}).catch((err) => {
+    console.log(err)
 })
